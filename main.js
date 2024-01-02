680abbecd2096785c8d7033e30491e6f2b8899b6
cam0studios <90651585+cam0studios@users.noreply.github.com>
2024-01-02 17:43:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 if(!Object.hasOwn(window.executed)) {
+  var script=document.createElement("script");script.src="https://cdn.jsdelivr.net/npm/eruda";document.body.append(script);script.onload=function(){eruda.init();console.log("Script Loaded: Thank you for using SparXSS!")}
   window.executed = true;
   let btn = document.createElement("button");
   btn.innerHTML = "Open Google";
