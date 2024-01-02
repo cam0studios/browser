2f07c4e4ca4805fd71a83474e2a807cc3765e789
cam0studios <90651585+cam0studios@users.noreply.github.com>
2024-01-02 16:53:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-window.open("https://google.com")
+if(!Object.hasOwn(window.executed)) {
+  window.executed = true;
+  let btn = document.createElement("button");
+  btn.innerHTML = "Open Google";
+  btn.addEventListener("click",() => {
+    window.open("https://google.com");
+  });
+  document.body.appendChild(btn);
+}
