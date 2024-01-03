cf35fe715622e93b60db89bcb3307e2bc0d9775d
cam0studios <90651585+cam0studios@users.noreply.github.com>
2024-01-03 08:32:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert(run);
+alert("run");
 if(!Object.hasOwn(window,executed)) {
   var script=document.createElement("script");script.src="https://cdn.jsdelivr.net/npm/eruda";document.body.append(script);script.onload=function(){eruda.init();console.log("Script Loaded: Thank you for using SparXSS!")}
   window.executed = true;
