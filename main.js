9c7067e1df2503bac81670a072ec4cfdee1235df
cam0studios <90651585+cam0studios@users.noreply.github.com>
2024-01-10 15:12:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 if(typeof executed === 'undefined') {
   executed = true;
-  document.write("it works");
+    (function(){var script=document.createElement("script");script.src="https://cdn.jsdelivr.net/npm/eruda";document.body.append(script);script.onload=function(){eruda.init();console.log("Script Loaded: Thank you for using SparXSS!")}})();
 }
